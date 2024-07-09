2b561f5be5393dbb249debd6f328582e63cc7b51
puraGH <3368746728@qq.com>
2024-07-09 13:51:52 +0800

    Add files via upload

---
+++ temp1.py
@@ -0,0 +1,12 @@
+class calculate():
+
+    def count(self, x1,x2):
+        print(x1/(x2 * x2))
+
+
+
+
+
+x1 = int(input('请输入你的体重（千克）：'))
+x2 = int(input('请输入你的身高（米）：'))
+calculate1 = calculate().count(x1, x2)
